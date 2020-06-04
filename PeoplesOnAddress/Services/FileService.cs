1b2f15fa170bad35670ebf516adc6f73466852f6
Johan Blomgren <31649682+JohanBlomgrenAvensia@users.noreply.github.com>
2020-06-04 21:13:01 +0200

    encoding

--- PeoplesOnAddress/Services/FileService.cs
+++ PeoplesOnAddress/Services/FileService.cs
@@ -2,9 +2,11 @@
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 using CsvHelper;
 using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualBasic.FileIO;
@@ -27,21 +29,42 @@ namespace PeoplesOnAddress.Services
             {
                 using (TextReader reader = new StreamReader(file.OpenReadStream()))
                 {
-                    //TODO CHECK IF VALID CSV
-                    var csvReader = new CsvReader(reader, configuration: new CsvConfiguration(CultureInfo.InvariantCulture)
-                    {
-                        Delimiter = ";"
-                    });
-                    //csvReader.Configuration.PrepareHeaderForMatch = (string header, int index) => header.ToLower();
-                    //csvReader.Configuration.HasHeaderRecord = true;
-                    csvReader.Configuration.RegisterClassMap<AddressCsvMap>();
-                    var records = csvReader.GetRecords<AddressCsvMap>();
+                    var readerString = reader.ReadToEnd();
 
-                    
-                    //_logger.LogError(ex, "Could not parse file");
-                    if (AnalyzerFileReference(file))
+                    Encoding encoding = Encoding.Unicode;
+
+                    var bytes = Encoding.Convert(Encoding.UTF8, Encoding.Unicode, encoding.GetBytes(readerString));
+
+                    var utf8 = Encoding.UTF8.GetString(encoding.GetBytes(readerString));
+                    var unicode = Encoding.Unicode.GetString(encoding.GetBytes(readerString));
+                    var def = Encoding.Default.GetString(encoding.GetBytes(readerString));
+                    var asc = Encoding.ASCII.GetString(encoding.GetBytes(readerString));
+
+
+
+                    var memStream = new MemoryStream(bytes);
+                    using (TextReader reader2 = new StreamReader(memStream))
                     {
-                        success = true;
+
+
+
+                        //TODO CHECK IF VALID CSV
+                        var csvReader = new CsvReader(reader2, configuration: new CsvConfiguration(CultureInfo.InvariantCulture)
+                        {
+                            Delimiter = ";"
+
+                        });
+                        //csvReader.Configuration.PrepareHeaderForMatch = (string header, int index) => header.ToLower();
+                        //csvReader.Configuration.HasHeaderRecord = true;
+                        //csvReader.Configuration.RegisterClassMap<AddressCsvMap>();
+                        var records = csvReader.GetRecords<AddressCsv>();
+
+
+                        //_logger.LogError(ex, "Could not parse file");
+                        if (AnalyzerFileReference(file))
+                        {
+                            success = true;
+                        }
                     }
                 }
             }
@@ -59,13 +82,13 @@ namespace PeoplesOnAddress.Services
     {
         public AddressCsvMap()
         {
-            Map(m => m.FirstName).Name("Förnamn");
-            Map(m => m.LastName).Name("Efternamn");
-            Map(m => m.PersonNumber).Name("Personnummer");
-            Map(m => m.Address).Name("Adress");
-            Map(m => m.ApartmentNumber).Name("Lägenhetsnummer");
-            Map(m => m.PostalCode).Name("Postnummer");
-            Map(m => m.City).Name("Stad");
+            Map(m => m.FirstName).Name("förnamn");
+            Map(m => m.LastName).Name("efternamn");
+            Map(m => m.PersonNumber).Name("personnummer");
+            Map(m => m.Address).Name("adress");
+            Map(m => m.ApartmentNumber).Name("lägenhetsnummer");
+            Map(m => m.PostalCode).Name("postnummer");
+            Map(m => m.City).Name("stad");
 
         }
        
@@ -73,18 +96,19 @@ namespace PeoplesOnAddress.Services
 
     public class AddressCsv
     {
+        [Name("Förnamn")]
         public string FirstName { get; set; }
-        
+        [Name("Efternamn")]
         public string LastName { get; set; }
-        
+        [Name("Personnummer")]
         public string PersonNumber { get; set; }
-        
+        [Name("Adress")]
         public string Address { get; set; }
-        
+        [Name("Lägenhetsnummer")]
         public string ApartmentNumber { get; set; }
-        
+        [Name("Postnummer")]
         public string PostalCode { get; set; }
-        
+        [Name("Stad")]
         public string City { get; set; }
     }
 }
